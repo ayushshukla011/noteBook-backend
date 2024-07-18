3678a131761309baacb204644571bc70c8e49722
Ayush shukla <ayushshukla3999@gmail.com>
2024-07-18 16:06:43 +0530

    done

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,21 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-const fetchuser = (req,res,next) => {
-  //Get the user from the jst token and add id to req object
-  const token = req.header("auth-token");
-
+const fetchuser = (req, res, next) => {
+  // Get the user from the JWT token and add id to req object
+  const authHeader = req.header("Authorization");
+	const token = authHeader && authHeader.split(" ")[1];
+  
+  
   if (!token) {
-    res.status(401).send({ error: "please authenticate using a valid token" });
+    return res.status(401).send({ error: "Please authenticate using a valid token" });
   }
 
   try {
     const data = jwt.verify(token, process.env.JWT_SECRET);
-    // console.log(data);data ko hi ham pass kar rahe the jwt me isliye data hi wapis milega uske andar user name ka ek object hai.
     req.user = data.user;
-    // console.log(data.user);
     next();
   } catch (error) {
-    res.status(401).send({ error: "please authenticate useing a valid token" });
+    console.error(`Token verification failed: ${error.message}`);
+    res.status(401).send({ error: "Please authenticate using a valid token" });
   }
 };
 
